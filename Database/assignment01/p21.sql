7e905a261fda8e9832c989d8e18bb0dc0b99d050
Prasad Tajane <tajane.pr@husky.neu.edu>
2017-12-15 21:36:55 -0500

    Added all semster material

--- Database/assignment01/p21.sql
+++ Database/assignment01/p21.sql
@@ -1,5 +1,27 @@
-SELECT		alb.title as album_title
-FROM 		Album alb INNER JOIN Artist a 
-				ON alb.ArtistId = a.ArtistId
-WHERE 	a.Name like '%Led Zeppelin%'
-ORDER BY	alb.title ASC
+
+CREATE TABLE Trainly.Member (
+
+    firstName VARCHAR(20),
+    lastName VARCHAR(50),
+    marital_status  VARCHAR(50),
+    whether_dependent  VARCHAR(50),
+    home_phone_number  VARCHAR(50),
+    
+    group_id  int,
+    plan_id  int,
+    contact_id  int,
+    member_id int,
+    
+    PRIMARY KEY (member_id)
+);
+
+
+select * from Trainly.Member;
+
+INSERT INTO `Trainly`.`Member` (`firstName`, `lastName`, `marital_status`, `whether_dependent`, `home_phone_number`, `group_id`, `plan_id`, `contact_id`) 
+VALUES ('pq', 'pq', 'pq', 'pq', 'pq', '12', '12', '12');
+
+INSERT INTO `Trainly`.`Member` (`firstName`, `lastName`, `marital_status`, `whether_dependent`, `home_phone_number`, `group_id`, `plan_id`, `contact_id`) 
+VALUES ('p', 'p', 'p', 'p', 'p', '1', '1', '1');
+
+delete from Member where member_id = 12
---
+++ Database/assignment05/schema.sql
@@ -0,0 +1,11 @@
+CREATE TABLE data (
+  a0 INT,
+  a1 INT PRIMARY KEY,
+  a2 INT,
+  a5 INT,
+  a10 INT,
+  a50 INT,
+  a100 INT,
+  a500 INT,
+  a1000 INT
+);
